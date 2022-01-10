6855db0366b8688881777f2f8a10e5a0c9747d46
mohamed085 <mohamed085555@gmail.com>
2022-01-10 15:33:35 +0200

    Enable Cross origin

--- backend/src/main/java/com/demo/ecommerce/dao/ProductCategoryRepository.java
+++ backend/src/main/java/com/demo/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.demo.ecommerce.dao;
 import com.demo.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/src/main/java/com/demo/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/demo/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.demo.ecommerce.dao;
 
 import com.demo.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
