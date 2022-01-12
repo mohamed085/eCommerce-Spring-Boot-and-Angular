3bb1e799a444f01dab562940ead32f6e50035861
mohamed085 <mohamed085555@gmail.com>
2022-01-12 11:06:48 +0200

    Display cart details and add increment and decrement items and remove

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 import { ProductService } from "./services/product.service";
 import { CartService } from "./services/cart.service";
@@ -13,14 +15,14 @@ import { CartService } from "./services/cart.service";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule, Routes } from "@angular/router";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 const routes: Routes = [
-  { path: 'search/:keyword', component: ProductListComponent },
-  { path: 'category/:id', component: ProductListComponent },
-  { path: 'category', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'cart-details', component: CartDetailsComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full'},
   { path: '**', redirectTo: '/products', pathMatch: 'full'},
 ]
@@ -32,7 +34,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     NgbModule,
